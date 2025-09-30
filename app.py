e8b59e17834324ba630050c9e508c2190bcf3248
CristianMB2255 <crbrun742@gmail.com>
2025-09-30 15:32:49 -0300

    POST method implementation

--- app.py
+++ app.py
@@ -30,7 +30,25 @@ def aluno():
             return({ 'error': 'Student not found or name malformed.', 'status': 400 })
         
         return aluno
-    
+
+    if request.method == 'POST':
+        if not request.json:
+            return({ 'error': 'Please pass a student JSON as argument.', 'status': 400 })
+         
+        with open('alunos.json') as fr:
+            data = json.load(fr)
+
+        if not fr:
+            return({ 'error': 'Error trying to open the file.', 'status': 500 })
+        
+        for aluno in request.json["alunos"]:
+            data["alunos"].append(aluno)
+
+        with open('alunos.json', 'w') as fw:
+            data = json.dump(data, fw, indent=4)
+
+        return data
+
 
 if __name__ == "__main__":
     app.run(debug=True)
