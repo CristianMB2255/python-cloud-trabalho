ed8bdb199c1f2e2629c7c12e47b0aeff6eb6c393
CristianMB2255 <crbrun742@gmail.com>
2025-09-30 16:22:50 -0300

    Added route 'github-repo'

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes.alunos import alunos_bp
 from routes.saudacao import saudacao_bp
 from routes.curso import curso_bp
 from routes.weather import weather_bp
+from routes.gitrepo import get_repo_bp
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ app.register_blueprint(alunos_bp)
 app.register_blueprint(saudacao_bp)
 app.register_blueprint(curso_bp)
 app.register_blueprint(weather_bp)
+app.register_blueprint(get_repo_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ routes/gitrepo.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request, Blueprint
+from utils import open_db, get_aluno
+import requests
+
+app = Flask(__name__)
+
+get_repo_bp = Blueprint("/github-repo", __name__)
+
+@get_repo_bp.route('/github-repo')
+def github_repo():
+    aluno_req = request.args.get('aluno')
+    if not aluno_req:
+        return jsonify(error='Please provide a student name.'), 400
+
+    db = open_db()
+    aluno = get_aluno(aluno_req, db)
+
+    if not aluno:
+        return jsonify(error='Student not found or name malformed.'), 404
+
+    repo_link = aluno.get('github_repo')
+
+    if not repo_link:
+        return jsonify(error='Student does not have a registered repo.'), 400
+
+    response = requests.get(repo_link)
+    if response.status_code != 200:
+        return jsonify(error='Repo not found.'), 400
+    
+    data = response.json()
+
+    result = {
+        "name": data.get("name"),
+        "description": data.get("description"),
+        "stars": data.get("stargazers_count")
+    }
+
+    return jsonify(result)
