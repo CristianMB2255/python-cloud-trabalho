d04fcd38b0a8bc61aa82a3ded50c40ae7463a373
CristianMB2255 <crbrun742@gmail.com>
2025-09-30 15:52:39 -0300

    Added route 'curso'

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from routes.alunos import alunos_bp
 from routes.saudacao import saudacao_bp
+from routes.curso import curso_bp
 
 app = Flask(__name__)
 
 # Register routes
 app.register_blueprint(alunos_bp)
 app.register_blueprint(saudacao_bp)
+app.register_blueprint(curso_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ routes/curso.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request, Blueprint
+from utils import open_db, aluno_exists
+
+app = Flask(__name__)
+
+curso_bp = Blueprint("curso", __name__)
+
+@curso_bp.route('/curso')
+def curso():
+    aluno_req = request.args.get('aluno')
+    if not aluno_req:
+        return jsonify(error='Please provide a student name.'), 400
+
+    db = open_db()
+    
+    aluno = aluno_exists(aluno_req, db)
+    if not aluno:
+        return jsonify(error='Student not found or name malformed.'), 404
+
+    # Get student's course
+    curso = aluno.get('curso')
+    if not curso:
+        return jsonify(error='Student does not have a registered course.'), 400
+    
+    return jsonify(data=curso)
+    
