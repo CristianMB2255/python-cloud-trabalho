89a871d103bb196830465bb498f6d3db40931eed
CristianMB2255 <crbrun742@gmail.com>
2025-09-30 15:40:46 -0300

    Reformat code for cleanliness and consistency

--- app.py
+++ app.py
@@ -1,53 +1,59 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
 
+DB_FILE = 'alunos.json'
+
+# Checks the existence of a student
 def get_aluno(aluno_name, db):
     for aluno in db['alunos']:
         if (aluno['name']) == aluno_name:
                 return aluno
-        return False
+    return False
+
+# Opens file
+def open_db():
+    with open(DB_FILE) as fr:
+        return json.load(fr)
+    if not fr:
+        return jsonify(error='Error trying to open file.'), 500
+    
+#Saves database
+def save_db(db):
+    with open(DB_FILE, 'w') as fw:
+        json.dump(db, fw, indent=4)
+
 
 @app.route('/alunos', methods=['GET', 'POST'])
 def aluno():
+    db = open_db()
+
     if request.method == 'GET':
+
+        # Returns all the students if no args
         aluno_req = request.args.get('aluno')
-
         if not aluno_req:
-            return({ 'error': 'Please pass a student name as an argument.', 'status': 400 })
-
-        with open('alunos.json') as f:
-            db = json.load(f)
-
-        if not f:
-            return({ 'error': 'Error trying to open the file.', 'status': 500 })
+            return db['alunos']
         
-
+        # Returns a specific student
         aluno = get_aluno(aluno_req, db)
-
         if not aluno:
-            return({ 'error': 'Student not found or name malformed.', 'status': 400 })
+            return jsonify(error='Student not found or name malformed.'), 400
         
         return aluno
 
     if request.method == 'POST':
-        if not request.json:
-            return({ 'error': 'Please pass a student JSON as argument.', 'status': 400 })
-         
-        with open('alunos.json') as fr:
-            data = json.load(fr)
-
-        if not fr:
-            return({ 'error': 'Error trying to open the file.', 'status': 500 })
+        if not request.json or not request.json.get('alunos'):
+            return jsonify(error='Please pass a student JSON as argument.'), 400
         
-        for aluno in request.json["alunos"]:
-            data["alunos"].append(aluno)
+        # Put students in db
+        for aluno in request.json.get('alunos'):
+            if not get_aluno(aluno['name'], db):
+                db['alunos'].append(aluno)
 
-        with open('alunos.json', 'w') as fw:
-            data = json.dump(data, fw, indent=4)
-
-        return data
+        save_db(db)
+        return jsonify(data=db)
 
 
 if __name__ == "__main__":
